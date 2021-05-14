7e7eeaf4dc107e9a030ef623f1e08a6e9e5997af
DrNossberg <titou210@hotmail.fr>
2021-05-14 12:12:44 +0200

    [Completion] ShapeGeom

--- src/ShapeGeom.java
+++ src/ShapeGeom.java
@@ -11,6 +11,32 @@
 **    Implementation of the Shape object
 */
 
-abstract class Shape {
-    
+
+import java.util.Observable;
+
+abstract class ShapeGeom extends Observable {
+    private Color color;
+    private Color borderColor;
+    private int thickness;
+    private Point center; // hum ?
+    private ArrayList<Point> pointMemo;
+    private ArrayList<boolean> showedMemo;
+
+    abstract void translate(int offset_x, int offest_y) {
+        for (Point memo : pointMemo)
+            memo.translate(offset_x, offest_y);
+    }
+    public void colorChange(Color);
+    public void modifyShape();
+    public ArrayList<Point> getpointMemo() {
+        ArrayList<Point> tmp = new ArrayList<Point>;
+
+        for (int i = 0; i < pointMemo.size(); i++)
+            if (showedMemo.get(i))
+                tmp.add(pointMemo.get(i));
+        return (tmp);
+    }
+    public void draw(Graphic g);
+
+
 }
