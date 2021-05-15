3a11ef7f0e0fe0d12e8ee607c57f2b48b29f3383
DrNossberg <titou210@hotmail.fr>
2021-05-15 22:54:26 +0200

    [Mediator/CanvasController] Implementing mouse motion
    - Mouse tracking is now a thing
    - Squares still bug but they will now be shaw anyway and right

--- src/Controllers/CanvasController.java
+++ src/Controllers/CanvasController.java
@@ -1,32 +1,58 @@
 package Controllers;
 
 import Models.*;
-import Models.Rectangle;
+import Models.Square;
+import Models.ShapeGeom;
+import Models.ShapeType;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
-public class CanvasController extends MouseAdapter {
-    private final Mediator m;
+
+
+public class CanvasController extends MouseAdapter  implements MouseListener, MouseMotionListener {
     private final ArrayList<Point> points = new ArrayList<>();
+    private final Mediator m;
+    private ShapeType shapeType = ShapeType.RECTANGLE;
+    private boolean finished = true;
 
     public CanvasController(Mediator m) {
         super();
         this.m = m;
+
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        if (!finished)
+            System.out.println("here!");
+        System.out.println("ladz!");
+
+    //     // repaint();
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        // System.out.println("fezfs");
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        System.out.println("hereqfefe");
     }
 
     public void mousePressed(MouseEvent e) {
-        if (SwingUtilities.isLeftMouseButton(e)) {
-            System.out.println("left mouse?");
-            points.add(new Point(e.getX(), e.getY()));
-
-            if (this.points.size() == 2) {
-                this.m.addShape(new Rectangle(points.get(0), points.get(1)));
-                this.points.clear();
-            }
+        if (!SwingUtilities.isLeftMouseButton(e))
+            return;
+        this.points.add(new Point(e.getX(), e.getY()));
+        this.finished = false;
+        // System.out.println("Points ! : " +this.points.size()  + " max : " + this.shapeType.getMaxMemoPoint());
+        if (this.points.size() == this.shapeType.getMaxMemoPoint()) {
+            this.m.addShape(new Square(points.get(0), points.get(1)));
+            this.points.clear();
+            this.finished = true;
         }
     }
 
